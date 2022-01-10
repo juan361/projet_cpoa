89878416aeecd6adc633f2f7a691378a5f70823a
atlas sanchez <jgsan360@gmail.com>
2022-01-10 14:57:15 +0100

    Update Planning.java

--- planningMatchs/Planning.java
+++ planningMatchs/Planning.java
@@ -6,6 +6,7 @@
 
 package planningMatchs;
 
+import java.sql.Date;
 import java.util.*;
 
 /** @pdOid 77a5cdea-6645-4d8f-a99d-427c7ea668bd */
@@ -13,9 +14,9 @@ public class Planning {
    /** @pdOid 371699e3-e105-48e8-bfcd-b9c35c7907ef */
    private int idPlanning;
    /** @pdOid 866136da-431c-4123-8d4b-96805607220e */
-   private date dateDabut;
+   private Date dateDebut;
    /** @pdOid 4ca0832f-c3c1-4abd-97ee-2ea0f63f8f3d */
-   private date dateFin;
+   private Date dateFin;
    /** @pdOid 1653f9d1-e67e-471a-81b4-30f381011cd2 */
    private String libelle;
 

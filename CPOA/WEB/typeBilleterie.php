a589987c4ec9df80bac474aa7f39481ca59004a7
AnneSoso <beal.annesophie@gmail.com>
2022-01-21 21:31:39 +0100

    New page typeBilleterie.php + images

---
+++ CPOA/WEB/typeBilleterie.php
@@ -0,0 +1,42 @@
+<?php
+session_start();
+include_once('header.php');
+include_once('footer.php');
+?>
+<!DOCTYPE html>
+    <body>
+        <div class="conteneurTypeBillets">
+            <div class="conteneurTypeBilletsdiv">
+                <a href="billeterie.php">
+                    <img src="OP1.jpg" style="width:100%; height:100%;" alt="image d'accueil" />
+                    <div>
+                        <p>Billets Grands Publics</p>
+                    </div>
+                </a>
+            </div>
+            <div class="conteneurTypeBilletsdiv">
+                <a href="billeterie.php">
+                    <img src="OP2.jpg" style="width:100%; height:100%;" alt="image d'accueil" />
+                    <div>
+                        <p>Billets Licenciés</p>
+                    </div>
+                </a>
+            </div>
+            <div class="conteneurTypeBilletsdiv">
+                <a href="billeterie.php">
+                    <img src="OP3.jpg" style="width:100%; height:100%;" alt="image d'accueil" />
+                    <div>
+                        <p>Billets de Solidarité</p>
+                    </div>
+                </a>
+            </div>    
+            <div class="conteneurTypeBilletsdiv">
+                <a href="billeterie.php">
+                    <img src="OP4.jpg" style="width:100%; height:100%;" alt="image d'accueil" />
+                    <div>
+                        <p>Billets "The Big Match"</p>
+                    </div>
+                </a>
+            </div>            
+        </div>
+</body>

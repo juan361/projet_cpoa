e42995de8f66c1c6c9b4a1e72bcc90f521a01bd3
Léna Iglesis <lena.iglesis@etu.univ-lyon1.fr>
2022-01-24 14:37:16 +0100

    Update JoueurDAO.java

--- Java/Planning_Matchs/src/DAO/JoueurDAO.java
+++ Java/Planning_Matchs/src/DAO/JoueurDAO.java
@@ -10,12 +10,15 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class JoueurDAO {
    private final Connection connexion;
    public JoueurDAO(Connection c){
       this.connexion = c;
    }
+
    public Joueur getJoueur(int id) throws SQLException {
       String rqt = "SELECT * FROM Joueur where id_joueur = ?";
       PreparedStatement state = connexion.prepareStatement(rqt);
@@ -24,5 +27,17 @@ public class JoueurDAO {
       result.next();
       Joueur j = new Joueur(result.getString("nom"), result.getString("prenom"), result.getInt("atp"), result.getString("nationalite"), result.getInt("id_joueur"));
       return j;
+   }
+
+   public List<Joueur> getListJoueurs() throws SQLException {
+      List<Joueur> joueursList = null;
+      String rqt = "SELECT * FROM Joueur";
+      PreparedStatement state = connexion.prepareStatement(rqt);
+      ResultSet result = state.executeQuery();
+      while(result.next()){
+         Joueur j = new Joueur(result.getString("nom"), result.getString("prenom"), result.getInt("atp"), result.getString("nationalite"), result.getInt("id_joueur"));
+         joueursList.add(j);
+      }
+      return joueursList;
    }
 }

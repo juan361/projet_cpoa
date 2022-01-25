0f2ade82aef1dfa1c059c25584519d3d678575ba
atlas Sanchez <jgsan360@gmail.com>
2022-01-25 11:01:26 +0100

    ça "marche"

--- Java/Planning_Matchs/src/Vue/PMatch.java
+++ Java/Planning_Matchs/src/Vue/PMatch.java
@@ -1,6 +1,10 @@
 package Vue;
 
+import Planning_Match.Joueur;
+
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class PMatch extends JFrame {
     private JTextField prenomTF;
@@ -11,12 +15,29 @@ public class PMatch extends JFrame {
     private JButton clearB;
     private JButton validerB;
     private JPanel main;
+    private JTextField classTF;
+    private JLabel classLabel;
 
-    public PMatch(){
+    public PMatch() {
         setContentPane(main);
-        setSize(450,300);
+        setSize(450, 300);
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         setVisible(true);
+        validerB.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                Joueur joueur = new Joueur(nomTF.getText(), prenomTF.getText(), Integer.parseInt(classTF.getText()), natTF.getText());
+            }
+        });
+        clearB.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                prenomTF.setText(" ");
+                nomTF.setText(" ");
+                natTF.setText(" ");
+                classTF.setText(" ");
+            }
+        });
     }
 
     public static void main(String[] args) {
